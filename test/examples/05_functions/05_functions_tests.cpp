66852fbc0a580906ea8b157744d35c335d3f2ca8
Samuel-Ellis <135079978+Samuel-Ellis@users.noreply.github.com>
2023-06-09 01:20:40 +0000

    Default functions + overloading + test

--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "val_ref.h"
+#include "default.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -14,4 +15,12 @@ TEST_CASE("Test referance param"){
 	auto num1 = 10;
 	reference_param(num1);
 	REQUIRE(num1 == 20);
+}
+TEST_CASE("Test function default parameters"){
+	REQUIRE(add_numbers(5, 10) == 15);
+	REQUIRE(add_numbers(5) == 15);
+}
+TEST_CASE("Test function default parameters int double int double"){
+	REQUIRE(add_numbers(5, 10.5) == 55);
+
 }
