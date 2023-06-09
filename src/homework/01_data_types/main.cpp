6c1b57b549cb9a545ace0148792d70f4bc4dac06
Samuel-Ellis <135079978+Samuel-Ellis@users.noreply.github.com>
2023-06-09 01:27:57 +0000

    Homework #1 Code and Test case

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,21 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "data_types.h"
 
 //write namespace using statement for cout
-
+using std::cin;
+using std::cout;
 
 int main()
 {
-	
+	int num;
+	int num1 = 4;
+	cout<<"Enter a number you would like to multiply by 5: ";
+	cin>>num;
+	int result = multiply_numbers(num);
+	cout<<"The result of "<<num<<" * 5 is: "<<result<<"\n";
+	result = multiply_numbers(num1);
+	cout<<"The result of 4 * 5 is: "<<result<<"\n";
 
 	return 0;
 }
