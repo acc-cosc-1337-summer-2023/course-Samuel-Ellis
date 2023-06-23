bb2db3cb108be128a220c76132a3d6edbc34eca4
Samuel-Ellis <135079978+Samuel-Ellis@users.noreply.github.com>
2023-06-23 01:07:39 +0000

    unique ptr example

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <memory>
+
+using std::unique_ptr; using std::make_unique;
 using std::cout;
+
 int main() 
 {	
+	/*//Manual non automatic
 	//1- create memory
-	int* int_ptr = new int(10); //creates memory on the heap dynamically
 	//2- use memory
-	cout<<*int_ptr<<"\n";
 	//3- free or delete memory from the heap/free store
-	delete int_ptr;
-	
+	int* int_ptr = new int(10); //creates memory on the heap dynamically
+	cout<<*int_ptr<<"\n";
+	delete int_ptr;*/
+
+	//Smart pointers(automatic)
+	unique_ptr<int> num = make_unique<int>(25);
+	cout<<*num<<"\n";
+
+
 	return 0;
 }
