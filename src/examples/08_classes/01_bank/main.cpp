8bfedc90e647cc6842cbc88671a4475cb0888730
Samuel-Ellis <135079978+Samuel-Ellis@users.noreply.github.com>
2023-06-25 04:18:41 +0000

    Adding unique ptr to classes example

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -13,10 +13,19 @@ using std::vector;
 int main()
 {
 	std::vector<unique_ptr<BankAccount>> accounts;
-	CheckingAccount checking(100) ;
 
 	accounts.push_back(make_unique<CheckingAccount>(100));
 	accounts.push_back(make_unique<SavingsAccount>(300));
+
+	unique_ptr<BankAccount> checking = make_unique<CheckingAccount>(200);
+	unique_ptr<BankAccount> savings = make_unique<SavingsAccount>(400);
+
+	accounts.push_back(std::move(checking));
+	accounts.push_back(std::move(savings));
+
+	//After the move checking and savings unique_ptrs are invalided
+	//they dont referance any data; if you use them program generates memory error
+	//cout<<checking->get_balance()<<"\n"; -- Error: Segmentation fault (core dumped)
 
 	run_menu(accounts);
 
