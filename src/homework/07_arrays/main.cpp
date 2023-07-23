dcfca2754b5daab296db8332cc1cb99cf4ed94e9
Samuel-Ellis <135079978+Samuel-Ellis@users.noreply.github.com>
2023-07-23 03:03:15 +0000

    Homework 8 code without testcase

--- src/homework/07_arrays/main.cpp
+++ src/homework/07_arrays/main.cpp
@@ -1,4 +1,17 @@
+#include "arrays.h"
+#include<iostream>
+using std::cout;
 int main()
 {
+    const int SIZE = 5;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	array_list.Add(5);
+	array_list.Add(7);
+	array_list.Add(10);
+	array_list.Add(15);
+	array_list.Add(20);
+	array_list.Delete(5);
     return 0;
 }
